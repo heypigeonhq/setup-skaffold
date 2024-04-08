03574779354b7972b708e757ff531e4365dba33d
Anthony Smith <420061+anthonator@users.noreply.github.com>
2024-04-08 11:17:33 -0400

    chore: fixed formatting

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,4 +1,4 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
-  ignores: [(message) => /^chore\(deps(-\S+)?\): bump/],
+  ignores: [(message) => /^chore\(deps(-\S+)?\): bump/m.test(message)],
 };
